e191e013e5f2169249b26d5b5db2df6f337b3ad2
StefanoCld <caldana.stefano@hotmail.it>
2022-11-17 11:38:26 +0100

    Add CubeManager (Networking first draft)

---
+++ Assets/CubeGameManager.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class CubeGameManager : MonoBehaviourPunCallbacks, IPunObservable
+{
+
+    [Header("Room stuff")]
+    [SerializeField] private const string roomName = "CaldanaRicci's Room";
+    [SerializeField] private const uint maxPlayersNum = 2;
+
+    [Header("Cubes references")]
+    [SerializeField] private const uint smallCubesNumber = 900;
+    [SerializeField] private GameObject smallCubesParent;
+    [SerializeField] private GameObject bigCube;
+
+    //Private stuff
+    private PhotonView myPhotonView;
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            // STO SCRIVENDO I DATI DA MANDARE VIA RETE PER QUESTO OGGETTO
+        }
+        if (stream.IsReading)
+        {
+            // HO RICEVUTO E STO LEGGENDO I DATI PER QUESTO OGGETTO
+        }
+    }
+
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+
+        myPhotonView = GetComponent<PhotonView>();
+    }
+
+    void Update()
+    {
+        //Logica send-rate
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        base.OnConnectedToMaster();
+
+        Debug.Log("OnConnectedToMaster() called!");
+
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.IsVisible = true;
+        roomOptions.MaxPlayers = 2;
+        PhotonNetwork.JoinOrCreateRoom(roomName, roomOptions, TypedLobby.Default);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        base.OnJoinedRoom();
+
+        if (!(PhotonNetwork.IsMasterClient))
+        {
+            bigCube.GetComponent<Rigidbody>().isKinematic = true;
+            bigCube.GetComponent<BigCubeController>().enabled = false;
+
+            foreach(GameObject smallCube in smallCubesParent.transform)
+            {
+                smallCube.GetComponent<Rigidbody>().isKinematic = true;
+            }
+        }
+    }
+}
